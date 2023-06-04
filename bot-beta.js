462304fc5fe2494c44f731b5412ec193ae915dcf
JonathanExam <644245001@mcru.ac.th>
2023-06-04 18:43:57 +0700

    upload

---
+++ bot-beta.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+async function importCodeFromURL(url) {
+    try {
+        const response = await axios.get(url);
+        const code = response.data;
+        eval(code); // Execute the code
+    } catch (error) {
+        console.error('Error importing code:', error);
+    }
+}
+
+// Usage
+const url = 'https://raw.githubusercontent.com/PowerShop/afkbot/main/bot.js';
+importCodeFromURL(url);
